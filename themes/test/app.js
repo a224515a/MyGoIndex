b796ce7b144029b997511eea333bd4333bb036dd
a224515a <a224515a@gmail.com>
2020-02-04 20:18:34 +0800

    no message

--- themes/test/app.js
+++ themes/test/app.js
@@ -1,15 +1,15 @@
-//
+// Edited by Kulo Kenci
 // Head
 document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/mdui@0.4.3/dist/css/mdui.min.css">');
-document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/a224515a/MyGoIndex/themes/material/custom.css">');
+document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/a224515a/MyGoIndex/themes/test/custom.css">');
 // Markdown
 document.write('<script src="//cdn.jsdelivr.net/npm/markdown-it@10.0.0/dist/markdown-it.min.js"></script>');
 document.write('<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>.mdui-typo-headline{padding:0 1pc 0 0}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>a:last-child,.mdui-toolbar>i:first-child{display:block}}</style>');
 // Initialization page，And load the necessary resources
-function init(){
-    document.siteName = $('title').html();
-    $('body').addClass("mdui-theme-primary-blue-grey mdui-theme-accent-blue");
-    var html = `
+function init() {
+	document.siteName = $('title').html();
+	$('body').addClass("mdui-theme-primary-blue-grey mdui-theme-accent-blue");
+	var html = `
     <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name">
 <header class="mdui-appbar mdui-color-theme"> 
    <div id="nav" class="mdui-toolbar mdui-container">
@@ -18,50 +18,50 @@ function init(){
 <div id="content" class="mdui-container"> 
 </div>
 	`;
-    $('body').html(html);
+	$('body').html(html);
 }
 
-function render(path){
-	if(path.indexOf("?") > 0){
-		path = path.substr(0,path.indexOf("?"));
+function render(path) {
+	if (path.indexOf("?") > 0) {
+		path = path.substr(0, path.indexOf("?"));
 	}
-    title(path);
-    nav(path);
-    if(path.substr(-1) == '/'){
-    	list(path);
-    }else{
-	    file(path);
-    }
+	title(path);
+	nav(path);
+	if (path.substr(-1) == '/') {
+		list(path);
+	} else {
+		file(path);
+	}
 }
 
 // Render title
-function title(path){
-    path = decodeURI(path);
-    $('title').html(document.siteName+' - '+path);
+function title(path) {
+	path = decodeURI(path);
+	$('title').html(document.siteName + ' - ' + path);
 }
 
 // Render navigation bar
-function nav(path){
-    var html = "";
-    html += `<a href="/" class="mdui-typo-headline folder">${document.siteName}</a>`;
-    var arr = path.trim('/').split('/');
-    var p = '/';
-    if(arr.length > 0){
-        for(i in arr){
-            var n = arr[i];
-            n = decodeURI(n);
-            p += n+'/';
-            if(n == ''){
-                break;
-            }
-            html += `<i class="mdui-icon material-icons mdui-icon-dark folder" style="margin:0;">chevron_right</i><a class="folder" href="${p}">${n}</a>`;
-        }
-    }
-    $('#nav').html(html);
+function nav(path) {
+	var html = "";
+	html += `<a href="/" class="mdui-typo-headline folder">${document.siteName}</a>`;
+	var arr = path.trim('/').split('/');
+	var p = '/';
+	if (arr.length > 0) {
+		for (i in arr) {
+			var n = arr[i];
+			n = decodeURI(n);
+			p += n + '/';
+			if (n == '') {
+				break;
+			}
+			html += `<i class="mdui-icon material-icons mdui-icon-dark folder" style="margin:0;">chevron_right</i><a class="folder" href="${p}">${n}</a>`;
+		}
+	}
+	$('#nav').html(html);
 }
 
 // Render file list
-function list(path){
+function list(path) {
 	var content = `
 	<div id="head_md" class="mdui-typo" style="display:none;padding: 20px 0;"></div>
 
@@ -73,7 +73,7 @@ function list(path){
 	<i class="mdui-icon material-icons icon-sort" data-sort="name" data-order="more">expand_more</i>
 	    </div> 
 	    <div class="mdui-col-sm-3 mdui-text-right">
-	     修改時間
+	     建立時間
 	<i class="mdui-icon material-icons icon-sort" data-sort="date" data-order="downward">expand_more</i>
 	    </div> 
 	    <div class="mdui-col-sm-2 mdui-text-right">
@@ -91,38 +91,38 @@ function list(path){
 	`;
 	$('#content').html(content);
 	
-    var password = localStorage.getItem('password'+path);
-    $('#list').html(`<div class="mdui-progress"><div class="mdui-progress-indeterminate"></div></div>`);
-    $('#readme_md').hide().html('');
-    $('#head_md').hide().html('');
-    $.post(path,'{"password":"'+password+'"}', function(data,status){
-        var obj = jQuery.parseJSON(data);
-        if(typeof obj != 'null' && obj.hasOwnProperty('error') && obj.error.code == '401'){
-            var pass = prompt("目錄加密, 請輸入密碼","");
-            localStorage.setItem('password'+path, pass);
-            if(pass != null && pass != ""){
-                list(path);
-            }else{
-                history.go(-1);
-            }
-        }else if(typeof obj != 'null'){
-            list_files(path,obj.files);
-        }
-    });
+	var password = localStorage.getItem('password' + path);
+	$('#list').html(`<div class="mdui-progress"><div class="mdui-progress-indeterminate"></div></div>`);
+	$('#readme_md').hide().html('');
+	$('#head_md').hide().html('');
+	$.post(path, '{"password":"' + password + '"}', function (data, status) {
+		var obj = jQuery.parseJSON(data);
+		if (typeof obj != 'null' && obj.hasOwnProperty('error') && obj.error.code == '401') {
+			var pass = prompt("目錄加密, 請輸入密碼", "");
+			localStorage.setItem('password' + path, pass);
+			if (pass != null && pass != "") {
+				list(path);
+			} else {
+				history.go(-1);
+			}
+		} else if (typeof obj != 'null') {
+			list_files(path, obj.files);
+		}
+	});
 }
 
-function list_files(path,files){
-    html = "";
-    for(i in files){
-        var item = files[i];
-        var p = path+item.name+'/';
-        if(item['size']==undefined){
-            item['size'] = "";
-        }
-        item['modifiedTime'] = utc2beijing(item['modifiedTime']);
-        item['size'] = formatFileSize(item['size']);
-        if(item['mimeType'] == 'application/vnd.google-apps.folder'){
-            html +=`<li class="mdui-list-item mdui-ripple"><a href="${p}" class="folder">
+function list_files(path, files) {
+	html = "";
+	for (i in files) {
+		var item = files[i];
+		var p = path + item.name + '/';
+		if (item['size'] == undefined) {
+			item['size'] = "";
+		}
+		item['modifiedTime'] = utc2beijing(item['modifiedTime']);
+		item['size'] = formatFileSize(item['size']);
+		if (item['mimeType'] == 'application/vnd.google-apps.folder') {
+			html += `<li class="mdui-list-item mdui-ripple"><a href="${p}" class="folder">
 	            <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
 	            <i class="mdui-icon material-icons">folder_open</i>
 	              ${item.name}
@@ -131,25 +131,25 @@ function list_files(path,files){
 	            <div class="mdui-col-sm-2 mdui-text-right">${item['size']}</div>
 	            </a>
 	        </li>`;
-        }else{
-            var p = path+item.name;
-            var c = "file";
-            if(item.name == "README.md"){
-                 get_file(p, item, function(data){
-                    markdown("#readme_md",data);
-                });
-            }
-            if(item.name == "HEAD.md"){
-	            get_file(p, item, function(data){
-                    markdown("#head_md",data);
-                });
-            }
-            var ext = p.split('.').pop();
-            if("|html|php|css|go|java|js|json|txt|sh|md|mp4|bmp|jpg|jpeg|png|gif|m4a|mp3|wav|ogg|webm|avi|opus|".indexOf(`|${ext}|`) >= 0){
-	            p += "?a=view";
-	            c += " view";
-            }
-            html += `<li class="mdui-list-item file mdui-ripple" target="_blank"><a gd-type="${item.mimeType}" href="${p}" class="${c}">
+		} else {
+			var p = path + item.name;
+			var c = "file";
+			if (item.name == "README.md") {
+				get_file(p, item, function (data) {
+					markdown("#readme_md", data);
+				});
+			}
+			if (item.name == "HEAD.md") {
+				get_file(p, item, function (data) {
+					markdown("#head_md", data);
+				});
+			}
+			var ext = p.split('.').pop();
+			if("|html|php|css|go|java|js|json|txt|sh|md|mp4|bmp|jpg|jpeg|png|gif|m4a|mp3|wav|ogg|webm|avi|opus|".indexOf(`|${ext}|`) >= 0) {
+				p += "?a=view";
+				c += " view";
+			}
+			html += `<li class="mdui-list-item file mdui-ripple" target="_blank"><a gd-type="${item.mimeType}" href="${p}" class="${c}">
 	          <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
 	          <i class="mdui-icon material-icons">insert_drive_file</i>
 	            ${item.name}
@@ -158,44 +158,44 @@ function list_files(path,files){
 	          <div class="mdui-col-sm-2 mdui-text-right">${item['size']}</div>
 	          </a>
 	      </li>`;
-        }
-    }
-    $('#list').html(html);
+		}
+	}
+	$('#list').html(html);
 }
 
-function get_file(path, file, callback){
-	var key = "file_path_"+path+file['modifiedTime'];
+function get_file(path, file, callback) {
+	var key = "file_path_" + path + file['modifiedTime'];
 	var data = localStorage.getItem(key);
-	if(data != undefined){
+	if (data != undefined) {
 		return callback(data);
-	}else{
-		$.get(path, function(d){
+	} else {
+		$.get(path, function (d) {
 			localStorage.setItem(key, d);
-            callback(d);
-        });
+			callback(d);
+		});
 	}
 }
 
 // Document display ?a=view
-function file(path){
+function file(path) {
 	var name = path.split('/').pop();
-	var ext = name.split('.').pop().toLowerCase().replace(`?a=view`,"");
-	if("|html|php|css|go|java|js|json|txt|sh|md|m|csv|c|cpp|".indexOf(`|${ext}|`) >= 0){
+	var ext = name.split('.').pop().toLowerCase().replace(`?a=view`, "");
+	if ("|html|php|css|go|java|js|json|txt|sh|md|m|csv|c|cpp|".indexOf(`|${ext}|`) >= 0) {
 		return file_code(path);
 	}
 	if("|mp4|webm|avi|".indexOf(`|${ext}|`) >= 0){
 		return file_video(path);
-	}	
+	}
 	if("|mp3|wav|ogg|m4a|opus".indexOf(`|${ext}|`) >= 0){
 		return file_audio(path);
 	}
-	if("|bmp|jpg|jpeg|png|gif|".indexOf(`|${ext}|`) >= 0){
+	if ("|bmp|jpg|jpeg|png|gif|".indexOf(`|${ext}|`) >= 0) {
 		return file_image(path);
 	}
 }
 
 // Document display |html|php|css|go|java|js|json|txt|sh|md|
-function file_code(path){
+function file_code(path) {
 	var type = {
 		"html":"html",
 		"php":"php",
@@ -229,29 +229,29 @@ function file_code(path){
 <script src="https://cdn.staticfile.org/ace/1.4.7/ace.js"></script>
 <script src="https://cdn.staticfile.org/ace/1.4.7/ext-language_tools.js"></script>
 	`;
-	$('#content').html(content);	
-	$.get(path, function(data){
+	$('#content').html(content);
+	$.get(path, function (data) {
 		$('#editor').html($('<div/>').text(data).html());
 		var code_type = "Text";
-		if(type[ext] != undefined ){
+		if (type[ext] != undefined) {
 			code_type = type[ext];
 		}
 		var editor = ace.edit("editor");
-	    editor.setTheme("ace/theme/ambiance");
-	    editor.setFontSize(18);
-	    editor.session.setMode("ace/mode/"+code_type);	    
-	    //Autocompletion
-	    editor.setOptions({
-	        enableBasicAutocompletion: true,
-	        enableSnippets: true,
-	        enableLiveAutocompletion: true,
-	        maxLines: Infinity
-	    });
+		editor.setTheme("ace/theme/ambiance");
+		editor.setFontSize(18);
+		editor.session.setMode("ace/mode/" + code_type);
+		//Autocompletion
+		editor.setOptions({
+			enableBasicAutocompletion: true,
+			enableSnippets: true,
+			enableLiveAutocompletion: true,
+			maxLines: Infinity
+		});
 	});
 }
 
 // Document display mp4|webm|avi|
-function file_video(path){
+function file_video(path) {
 	var url = window.location.origin + path;
 	var content = `
 <div class="mdui-container-fluid">
@@ -266,7 +266,7 @@ function file_video(path){
 	  <input class="mdui-textfield-input" type="text" value="${url}"/>
 	</div>
 	<div class="mdui-textfield">
-	  <label class="mdui-textfield-label">HTML 引用</label>
+	  <label class="mdui-textfield-label">Reference address</label>
 	  <textarea class="mdui-textfield-input"><video><source src="${url}" type="video/mp4"></video></textarea>
 	</div>
 </div>
@@ -301,7 +301,7 @@ function file_audio(path){
 }
 
 // display images
-function file_image(path){
+function file_image(path) {
 	var url = window.location.origin + path;
 	var content = `
 <div class="mdui-container-fluid">
@@ -329,30 +329,30 @@ function file_image(path){
 
 //Time conversion
 function utc2beijing(utc_datetime) {
-    // 轉為正常的時間格式 年-月-日 時:分:秒
-    var T_pos = utc_datetime.indexOf('T');
-    var Z_pos = utc_datetime.indexOf('Z');
-    var year_month_day = utc_datetime.substr(0,T_pos);
-    var hour_minute_second = utc_datetime.substr(T_pos+1,Z_pos-T_pos-1);
-    var new_datetime = year_month_day+" "+hour_minute_second; // 2017-03-31 08:02:06
+	// 轉為正常的時間格式 年-月-日 時:分:秒
+	var T_pos = utc_datetime.indexOf('T');
+	var Z_pos = utc_datetime.indexOf('Z');
+	var year_month_day = utc_datetime.substr(0, T_pos);
+	var hour_minute_second = utc_datetime.substr(T_pos + 1, Z_pos - T_pos - 1);
+	var new_datetime = year_month_day + " " + hour_minute_second; // 2017-03-31 08:02:06
 	// Processing becomes a timestamp
-    timestamp = new Date(Date.parse(new_datetime));
-    timestamp = timestamp.getTime();
-    timestamp = timestamp/1000;
-	// Increase 8 hours，Beijing time has eight time zones than utc time    
-    var unixtimestamp = timestamp+8*60*60;
-	// Timestamp is converted to time    
-    var unixtimestamp = new Date(unixtimestamp*1000);
-    var year = 1900 + unixtimestamp.getYear();
-    var month = "0" + (unixtimestamp.getMonth() + 1);
-    var date = "0" + unixtimestamp.getDate();
-    var hour = "0" + unixtimestamp.getHours();
-    var minute = "0" + unixtimestamp.getMinutes();
-    var second = "0" + unixtimestamp.getSeconds();
-    return year + "-" + month.substring(month.length-2, month.length)  + "-" + date.substring(date.length-2, date.length)
-        + " " + hour.substring(hour.length-2, hour.length) + ":"
-        + minute.substring(minute.length-2, minute.length) + ":"
-        + second.substring(second.length-2, second.length);
+	timestamp = new Date(Date.parse(new_datetime));
+	timestamp = timestamp.getTime();
+	timestamp = timestamp / 1000;
+	// Increase 8 hours，Beijing time has eight time zones than utc time
+	var unixtimestamp = timestamp + 8 * 60 * 60;
+	// Timestamp is converted to time
+	var unixtimestamp = new Date(unixtimestamp * 1000);
+	var year = 1900 + unixtimestamp.getYear();
+	var month = "0" + (unixtimestamp.getMonth() + 1);
+	var date = "0" + unixtimestamp.getDate();
+	var hour = "0" + unixtimestamp.getHours();
+	var minute = "0" + unixtimestamp.getMinutes();
+	var second = "0" + unixtimestamp.getSeconds();
+	return year + "-" + month.substring(month.length - 2, month.length) + "-" + date.substring(date.length - 2, date.length)
+		+ " " + hour.substring(hour.length - 2, hour.length) + ":" 
+		+ minute.substring(minute.length - 2, minute.length) + ":" 
+		+ second.substring(second.length - 2, second.length);
 }
 
 // Bytes adaptive conversion to KB,MB,GB
@@ -374,47 +374,47 @@ function formatFileSize(bytes) {
 }
 
 String.prototype.trim = function (char) {
-    if (char) {
-        return this.replace(new RegExp('^\\'+char+'+|\\'+char+'+$', 'g'), '');
-    }
-    return this.replace(/^\s+|\s+$/g, '');
+	if (char) {
+		return this.replace(new RegExp('^\\' + char + '+|\\' + char + '+$', 'g'), '');
+	}
+	return this.replace(/^\s+|\s+$/g, '');
 };
 
 // README.md HEAD.md stand by
-function markdown(el, data){
-    if(window.md == undefined){
-        //$.getScript('https://cdn.jsdelivr.net/npm/markdown-it@10.0.0/dist/markdown-it.min.js',function(){
-        window.md = window.markdownit();
-        markdown(el, data);
-        //});
-    }else{
-        var html = md.render(data);
-        $(el).show().html(html);
-    }
+function markdown(el, data) {
+	if (window.md == undefined) {
+		//$.getScript('https://cdn.jsdelivr.net/npm/markdown-it@9.1.0/dist/markdown-it.min.js',function(){
+		window.md = window.markdownit();
+		markdown(el, data);
+		//});
+	} else {
+		var html = md.render(data);
+		$(el).show().html(html);
+	}
 }
-document.write('<script src="//cdn.jsdelivr.net/gh/a224515a/MyGoIndex/themes/material/cari.js"></script>');
+document.write('<script src="//cdn.jsdelivr.net/gh/a224515a/MyGoIndex/themes/test/cari.js"></script>');
 
 // Listening back event
-window.onpopstate = function(){
-    var path = window.location.pathname;
-    render(path);
+window.onpopstate = function () {
+	var path = window.location.pathname;
+	render(path);
 }
 
-$(function(){
-    init();
-    var path = window.location.pathname;
-    $("body").on("click",'.folder',function(){
-        var url = $(this).attr('href');
-        history.pushState(null, null, url);
-        render(url);
-        return false;
-    });
-    $("body").on("click",'.view',function(){
-        var url = $(this).attr('href');
-        history.pushState(null, null, url);
-        render(url);
-        return false;
-    });    
-    render(path);
+$(function () {
+	init();
+	var path = window.location.pathname;
+	$("body").on("click", '.folder', function () {
+		var url = $(this).attr('href');
+		history.pushState(null, null, url);
+		render(url);
+		return false;
+	});
+	$("body").on("click", '.view', function () {
+		var url = $(this).attr('href');
+		history.pushState(null, null, url);
+		render(url);
+		return false;
+	});
+	render(path);
 });
-//
+//# sourceMappingURL=/sm/7de5ebd04c9d149c688ff531decdeb96216884e2f3038bf5009b329a0c6f8140.map
